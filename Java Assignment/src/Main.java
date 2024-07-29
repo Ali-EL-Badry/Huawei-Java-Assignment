4f5f351033e391846f3892af9809db0e28c310c9
rawan-ashraf <rawanashraf7776@gmail.com>
2024-07-29 14:07:15 +0300

    Update Main.java

--- Java Assignment/src/Main.java
+++ Java Assignment/src/Main.java
@@ -1,14 +1,7 @@
 import java.util.Scanner;
 import all.*;
 public class Main {
-         static char[][]grid=new char[10][10];
-      public static void grid(){
-       
-        for(int i=0;i<10;i++){
-            for(int j=0;j<10;j++){
-                grid[i][j]='*';
-            }
-        }}
+ 
     public static void main(String[] args) {
         while (true){
             // Main menu
