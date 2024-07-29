36a3788e8c321ca70f3b79417ef4c0aa25571d25
rawan-ashraf <rawanashraf7776@gmail.com>
2024-07-29 14:36:12 +0300

    Update Maze.java

--- Java Assignment/src/all/Maze.java
+++ Java Assignment/src/all/Maze.java
@@ -2,14 +2,7 @@ package all;
      
 public class Maze {
     private final char[][] maze = new char[10][10];
-   static char[][]grid=new char[10][10];
-      public static void grid(){
-       
-        for(int i=0;i<10;i++){
-            for(int j=0;j<10;j++){
-                grid[i][j]='*';
-            }
-        }}
+  
     public Maze() {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++)
