aea049c2b181e853c7d828c9b61acdd58aff24b4
Aibak <aibakgaming747@gmail.com>
2024-07-29 04:42:35 +0300

    Add files via upload

---
+++ Java Assignment/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import all.*;
+public class Main {
+    public static void main(String[] args) {
+        while (true){
+            // Main menu
+            System.out.println("# ===== Welcome to Maze Game ===== #");
+            System.out.println("           1. Start Game           ");
+            System.out.println("           2. Exit Game            ");
+            System.out.println("Your choice is : ");
+            Scanner input = new Scanner(System.in);
+            int choice = input.nextInt();
+
+            // The Main Game
+            if (choice == 1){
+                Maze maze = new Maze();
+                maze.printMaze();
+            }
+            // Exit game
+            else if (choice == 2){
+                System.out.println("Thank you for using Maze Game");
+                break;
+            }
+            // Validation of Main menu
+            else
+                System.out.println("Invalid choice");
+        }
+
+    }
+}
---
+++ Java Assignment/src/all/Maze.java
@@ -0,0 +1,20 @@
+package all;
+
+public class Maze {
+    private final char[][] maze = new char[10][10];
+
+    public Maze() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++)
+                this.maze[i][j] = '*';
+        }
+    }
+    public void printMaze() {
+        for (int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++)
+                System.out.print(this.maze[i][j]);
+            System.out.println();
+        }
+    }
+
+}
