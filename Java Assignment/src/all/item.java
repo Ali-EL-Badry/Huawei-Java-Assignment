444e688b5cdbb0f2e525f4517a9a0f5630a0ced5
Ahmed Tarek <143562670+ahmedtarek132@users.noreply.github.com>
2024-07-31 01:12:58 +0300

    Update item.java

--- Java Assignment/src/all/item.java
+++ Java Assignment/src/all/item.java
@@ -1,9 +1,20 @@
 package all;
 
-public abstract class item {
+public abstract class item extends Character {
   protected int x;
   public item(int  x)
     {
         this.x= x;
+    }
+  public iteem() {
+        Random r = new Random();
+        int x , y , z;
+        do {
+            x = r.nextInt(11);
+            y = r.nextInt(11);
+            z = r.nextInt(201);
+        } while (x == 0 && y == 0);
+
+        super(x, y, z);
     }
 }
