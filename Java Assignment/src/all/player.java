8348f8395a03037e1bbf2d5a9a49963367c1e9c9
Aibak <aibakgaming747@gmail.com>
2024-07-31 21:19:04 +0300

    Update player.java

--- Java Assignment/src/all/player.java
+++ Java Assignment/src/all/player.java
@@ -4,6 +4,14 @@ public class player extends Character {
     protected char name;
     protected int health;
 
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
     public player(String play) {
         super(0, 0, 10);
         this.name = play.charAt(0);
