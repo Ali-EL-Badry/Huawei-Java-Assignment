7c514e8aad02e696f005f9a2512779fa8aad1919
Aibak <aibakgaming747@gmail.com>
2024-07-31 21:18:50 +0300

    Update monster.java

--- Java Assignment/src/all/monster.java
+++ Java Assignment/src/all/monster.java
@@ -7,8 +7,8 @@ public class monster extends Character{
         Random rand = new Random();
         int x , y , z;
         do {
-            x = rand.nextInt(11);
-            y = rand.nextInt(11);
+            x = rand.nextInt(10);
+            y = rand.nextInt(10);
             z = rand.nextInt(201);
         } while (x == 0 && y == 0);
 
