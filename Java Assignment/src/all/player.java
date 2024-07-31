24875bb53f8faf07cfb5871557539d10f9c7e9bc
Aibak <aibakgaming747@gmail.com>
2024-07-31 18:23:50 +0300

    Update player.java

--- Java Assignment/src/all/player.java
+++ Java Assignment/src/all/player.java
@@ -1,35 +1,12 @@
 package all;
 
 public class player extends Character {
-    protected String name;
+    protected char name;
     protected int health;
 
-    public player(String name) {
+    public player(String play) {
         super(0, 0, 10);
-        this.name = name;
+        this.name = play.charAt(0);
         this.health = 100;
-    }
-
-    public void move(char direction) throws Exception {
-        switch (direction) {
-            case 'w':
-                if (x > 0) x--;
-                else throw new Exception("Cannot move up, out of bounds.");
-                break;
-            case 's':
-                if (x < 9) x++;
-                else throw new Exception("Cannot move down, out of bounds.");
-                break;
-            case 'a':
-                if (y > 0) y--;
-                else throw new Exception("Cannot move left, out of bounds.");
-                break;
-            case 'd':
-                if (y < 9) y++;
-                else throw new Exception("Cannot move right, out of bounds.");
-                break;
-            default:
-                throw new Exception("Invalid move direction.");
-        }
     }
 }
