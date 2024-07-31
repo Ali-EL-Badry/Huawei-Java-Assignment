3993b9ae7ef38d2543dada1b8e8fbdd4eda936c1
Aibak <aibakgaming747@gmail.com>
2024-07-31 21:17:44 +0300

    Update Character.java

--- Java Assignment/src/all/Character.java
+++ Java Assignment/src/all/Character.java
@@ -3,6 +3,15 @@ package all;
 public class Character {
     protected int x, y, power;
 
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+
+    public int getPower() {
+        return power;
+    }
+
     public Character(int x, int y, int power){
         this.x = x;
         this.y = y;
