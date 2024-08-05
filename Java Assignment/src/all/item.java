5a6a293c930881808bcbc0021ac8fecc8ba57235
Aibak <aibakgaming747@gmail.com>
2024-08-06 00:48:46 +0300

    Update item.java

--- Java Assignment/src/all/item.java
+++ Java Assignment/src/all/item.java
@@ -1,18 +1,17 @@
 package all;
 import java.util.Random;
 public class item{
-
     protected int x, y, z;
+
+    // Constructor of char
     public item() {
         Random r = new Random();
-        int x , y , z;
+        int x , y;
         do {
             x = r.nextInt(10);
             y = r.nextInt(10);
         } while (x == 0 && y == 0);
         this.x = x;
         this.y = y;
-
     }
-
 }
